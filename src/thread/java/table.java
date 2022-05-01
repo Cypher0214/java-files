23c5fa002a23138cc7300081b6953ebca2ca65d2
chinmay <pathakchinmay02@gmail.com>
2022-05-01 19:39:36 +0530

    java practise file

--- src/thread/java/table.java
+++ src/thread/java/table.java
@@ -2,18 +2,58 @@ package thread.java;
 
 public class table {
 
-        void print(int i)
-        {
-            for (i = 0; i < 10; i++)
-            {
-                System.out.println();
-            }
-        }
-}
- class thread1 extends Thread
-{
-    @Override
-    public void run() {
+    synchronized void print(int n) {
+        int i;
+        for (i = 1; i <=10; i++) {
+            System.out.println(n * i);
 
+
+            try {
+                Thread.sleep(500);
+
+            } catch (InterruptedException e) {
+                System.out.println("exception is" + e);
+            }
+
+        }
     }
 }
+    class thread1 extends Thread {
+        table t;
+
+        thread1(table t) {
+            this.t = t;
+
+        }
+
+        public void run() {
+            t.print(2);
+
+        }
+    }
+
+    class thread2 extends Thread {
+         table t;
+
+        thread2(table t) {
+            this.t = t;
+
+        }
+
+        public void run() {
+            t.print(5);
+
+        }
+    }
+class synchronised{
+    public static void main(String[] args) {
+        table obj=new table();
+        thread1 obj1=new thread1(obj);
+        thread2 obj2=new thread2(obj);
+        obj1.start();
+        obj2.start();
+    }
+}
+
+
+
